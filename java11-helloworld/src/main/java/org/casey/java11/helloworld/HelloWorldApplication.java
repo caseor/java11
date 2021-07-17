2eace58693539816fc41ee363500de09d5d120cb
Fu Kai <ca2pa23187@gmail.com>
2021-07-17 21:17:28 +0800

    init

---
+++ java11-helloworld/src/main/java/org/casey/java11/helloworld/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package org.casey.java11.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Fu Kai
+ * @since 20210717
+ */
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
---
+++ java11-helloworld/src/main/java/org/casey/java11/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package org.casey.java11.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @author Fu Kai
+ * @since 20210717
+ */
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping
+    public Map<String, Object> helloworld() {
+        return Map.of("data", "hello world", "msg", "success");
+    }
+}
