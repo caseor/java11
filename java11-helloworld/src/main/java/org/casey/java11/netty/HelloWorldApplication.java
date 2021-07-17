920402af6acd919f7f8263d1333f341c7db83d8b
Fu Kai <ca2pa23187@gmail.com>
2021-07-17 21:53:18 +0800

    add netty quarkus plan

--- java11-helloworld/src/main/java/org/casey/java11/netty/HelloWorldApplication.java
+++ java11-helloworld/src/main/java/org/casey/java11/netty/HelloWorldApplication.java
@@ -1,4 +1,4 @@
-package org.casey.java11.helloworld;
+package org.casey.java11.netty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- java11-helloworld/src/main/java/org/casey/java11/netty/controller/HelloWorldController.java
+++ java11-helloworld/src/main/java/org/casey/java11/netty/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package org.casey.java11.helloworld.controller;
+package org.casey.java11.netty.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
